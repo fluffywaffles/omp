9ee58940d7a1c0f8c4449eb323f83eb6a013b34f
fluffywaffles <skorlir@gmail.com>
2018-09-21 19:20:26 -0500

    Minor: add a note to hello.c, clean up slightly

--- hello.c
+++ hello.c
@@ -1,8 +1,9 @@
-#include "omp.h"
+#include <omp.h>
 #include <stdio.h>
 
 int main () {
   // Uses processor default (aka compiler default) max num threads
+  // On a Ryzen 5 1600 (my machine), that's 12 (0...11)
   #pragma omp parallel
   {
     int ID = omp_get_thread_num();
