9e8def5576abd53a749ed6fff71dfc18827a9684
fluffywaffles <skorlir@gmail.com>
2018-10-16 18:39:19 -0500

    Bugfix: corrected pi.c

--- pi.c
+++ pi.c
@@ -1,11 +1,10 @@
 #include <omp.h>
 #include <stdio.h>
 
-static long num_steps = 100000;
+static long num_steps = 1000000000;
 
-double step;
-
-/* A motivating example for OpenMP:
+/**
+ * A motivating example for OpenMP:
  *
  * We can calculate the exact value of π as:
  *   integral[ 4 / (1 + x^2)]
@@ -21,9 +20,14 @@ double step;
  * And use exactly one: #pragma omp parallel {...}
  */
 int main () {
-  int printed = 0;
-  double sum = 0.0;
-  step = 1.0 / (double) num_steps;
+  double sum  = 0.0;
+  double step = 1.0 / (double) num_steps;
+  int max_threads = omp_get_max_threads();
+  int actual_slice_count = -1;
+
+  printf("info: maximum usable threads: %d\n\n", max_threads);
+  double slices[max_threads];
+  for (int i = 0; i < max_threads; i++) slices[i] = 0.0;
 
   #pragma omp parallel
   {
@@ -32,25 +36,34 @@ int main () {
     int extra_slices      = num_steps % thread_count;
     int thread_slice_size = num_steps / thread_count;
 
+    if (actual_slice_count == -1) actual_slice_count = thread_count;
+
     int slice_start = id * thread_slice_size;
     int slice_end   = slice_start + thread_slice_size;
     if (id == (thread_count - 1)) {
-      printf("%2d: adding %d extra iterations to reach %ld\n", id, extra_slices, num_steps);
+      printf(
+        "thread(%2d): add %d iterations to reach %ld\n",
+        id, extra_slices, num_steps
+      );
       slice_end += extra_slices;
     }
 
-    printf("%2d: summing slice: %10d ‒ %10d\n", id, slice_start, slice_end);
+    printf(
+      "thread(%2d): summing slice: %10d ‒ %10d\n",
+      id, slice_start, slice_end
+    );
 
-    int i;
-    double x;
+    int i; double x;
     for (i = slice_start; i < slice_end; i++) {
       x = (i + 0.5) * step;
-      sum += 4.0 / (1.0 + x*x);
+      slices[id] += 4.0 / (1.0 + x*x);
     }
   }
 
+  for (int i = 0; i < actual_slice_count; i++) sum += slices[i];
+
   double pi = step * sum;
 
-  printf("\n\ncompleted summation.\n\n\n");
-  printf("pi after %ld steps ≈\n%1.80f\n\n", num_steps, pi);
+  printf("\ncompleted summation.\n\n");
+  printf("pi after %ld steps ≈\n%1.50f\n\n", num_steps, pi);
 }
